acdc083fff1cf3848ee046d55d074708c1d706b8
Shanna Sullivan <shanna.sullivan@udacity.com>
2018-09-08 16:23:32 -0700

    sort questions to show newest-created ones first

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,11 +1,25 @@
 import * as types from '../actions/types';
 
+// sort questions with newest-created questions showing first
+function sortQuestions(questions) {
+  const sortedQuestions = {};
+  const sortedIds = Object.keys(questions).sort((qid1, qid2) => {
+    return questions[qid1].timestamp < questions[qid2].timestamp;
+  });
+
+  sortedIds.forEach(id => sortedQuestions[id] = questions[id]);
+
+  return sortedQuestions;
+}
+
 export default function questions(state = {}, action) {
   switch(action.type) {
     case types.RECEIVE_QUESTIONS:
+      const sortedQuestions = sortQuestions(action.questions);
+
       return {
         ...state,
-        ...action.questions
+        ...sortedQuestions
       }
 
     default:
