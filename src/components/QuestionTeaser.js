44ccb60c0ba3db4621397f8422a47289dcffcc5d
Shanna Sullivan <shanna.sullivan@udacity.com>
2018-09-14 10:04:05 -0700

    add createdAt date to question teaser component

--- src/components/QuestionTeaser.js
+++ src/components/QuestionTeaser.js
@@ -9,13 +9,15 @@ export default class QuestionTeaser extends Component {
 
   render() {
     const { question } = this.props;
-    const { id, optionOne } = question;
+    const { id, optionOne, timestamp } = question;
+    const date = new Date(timestamp).toDateString();
 
     return (
       <Link to={`/question/${id}`} className="question-teaser-link">
         <li className="question-teaser">
           <p>Would You Rather:</p>
           <p>{optionOne.text} or...</p>
+          <p>created: {date}</p>
         </li>
       </Link>
     );
